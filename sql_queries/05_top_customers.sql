cf90a76856fd9baf746ea2c9c4f88cd0791bec19
Mickuye <mickuye13@gmail.com>
2025-09-20 15:30:21 +0100

    05_top_customers.sql

--- sql_queries/05_top_customers.sql
+++ sql_queries/05_top_customers.sql
@@ -1,6 +1,9 @@
 #5. Who are the top 10 customers by sales?
-SELECT Customer_Name, SUM(sales) AS Total_Sales
+SELECT 
+    Customer_Name, 
+    SUM(sales) AS Total_Sales,
+    SUM(profit) AS Total_Profit
 FROM superstore_orders
-GROUP BY customer_name
+GROUP BY Customer_Name
 ORDER BY Total_Sales DESC
 LIMIT 10;
