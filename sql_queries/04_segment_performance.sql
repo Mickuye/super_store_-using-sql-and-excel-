b35277b4ae5a64b836087e4639bfc06c88f64923
Mickuye <mickuye13@gmail.com>
2025-09-20 15:22:31 +0100

    04_segment_performance.sql

--- sql_queries/04_segment_performance.sql
+++ sql_queries/04_segment_performance.sql
@@ -1,10 +1,26 @@
 #4. Which customer segment generates the highest sales and profit?
-SELECT segment, SUM(sales) AS Total_Sales
-FROM superstore_orders
-GROUP BY segment
-ORDER BY Total_Sales DESC;
+WITH table1 AS (
+    SELECT 
+        segment, 
+        SUM(sales) AS Total_Sales
+    FROM superstore_orders
+    GROUP BY segment
+    ORDER BY Total_Sales DESC
+),
+table2 AS (
+    SELECT 
+        segment, 
+        SUM(profit) AS Total_Profit
+    FROM superstore_orders
+    GROUP BY segment
+    ORDER BY Total_Profit DESC
+)
+SELECT 
+    t1.segment,
+    t1.Total_Sales,
+    t2.Total_Profit
+FROM table1 t1
+JOIN table2 t2 
+    ON t1.segment = t2.segment
+ORDER BY t1.segment DESC;
 
-SELECT segment, SUM(Profit) AS Total_Profit
-FROM superstore_orders
-GROUP BY segment
-ORDER BY Total_Profit DESC;
