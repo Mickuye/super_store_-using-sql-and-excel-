a76ef8f99cb6c1a2736ced354bca7ae28fa5e459
Mickuye <mickuye13@gmail.com>
2025-09-25 14:45:09 +0100

    16_monthly_profit_change.sql

--- sql_queries/16_monthly_profit_change.sql
+++ sql_queries/16_monthly_profit_change.sql
@@ -1,4 +1,13 @@
 #16. What is the month-to-month percentage change in profit?
+ALTER TABLE superstore_orders
+ADD COLUMN Month_name varchar(11),
+ADD COLUMN Month int;
+
+UPDATE superstore_orders
+SET Month_name = monthname(Order_Date),
+	Month = month(Order_Date);
+
+  
 WITH table1 AS (
     SELECT YEAR(Order_Date) AS Year, MONTH(Order_Date) AS Month, MONTHNAME(Order_Date) AS Month_name, SUM(profit) AS profit
     FROM superstore_orders
